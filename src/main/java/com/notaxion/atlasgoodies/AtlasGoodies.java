470486462d51aaea1a3c6b57569e030fd6741170
mrnoone456 <mr.bhakat456@gmail.com>
2025-09-05 11:42:07 +0530

    initial commit: start of project just added the dep of atlas

---
+++ src/main/java/com/notaxion/atlasgoodies/AtlasGoodies.java
@@ -0,0 +1,23 @@
+package com.notaxion.atlasgoodies;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = AtlasGoodies.MODID, name = AtlasGoodies.NAME, version = AtlasGoodies.VERSION,
+        dependencies = "required-after:antiqueatlas")
+public class AtlasGoodies {
+    public static final String MODID = "atlasgoodies";
+    public static final String NAME = "Atlas Goodies";
+    public static final String VERSION = "1.0.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println("Atlas Goodies: Pre-initialization");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Atlas Goodies: Initialization");
+    }
+}
